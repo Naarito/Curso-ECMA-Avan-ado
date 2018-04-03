a67ba65d7b82916243c7681bb06e39d76db58ad7
Matheus Narito Yoneoka <matheus.yoneoka@hotmail.com>
2018-04-03 15:33:10 -0300

    Configurando o controller para capturar os dados da DOM

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,9 +1,19 @@
-class NegociacaoController {
+class NegociacaoController {        //CRIA UM CONTROLADOR PARA INTERAÇÃO COM USUÁRIO E DOM
 
-    adiciona(event) {
+    adiciona(event) {           //CRIA UM MÉTODO QUE RECEBE COMO ARGUMENTO O EVENTO UTILIZADO NO 
         
-        event.preventDefault();
+        event.preventDefault();                    //EVITA O RELOAD, AÇÃO PADRÃO DO EVENTO
         console.log('Controller Chamado!');
+
+        let $ = document.querySelector.bind(document);      //CRIA UM ATALHO PARA O QUERY SELECTOR
+
+        let inputData = $('#data');
+        let inputQuantidade = $('#quantidade');         //BUSCA OS CAMPOS PELOS SEUS IDENTIFICADORES
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
 
     }
 
