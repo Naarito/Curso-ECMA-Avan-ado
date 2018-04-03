ccfc95dfb6bbfa2265e9f5ba999ef3ea5968eb56
Matheus Narito Yoneoka <matheus.yoneoka@hotmail.com>
2018-04-03 16:40:04 -0300

    Transformando data de string para formato date - mais verboso

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -20,16 +20,7 @@ class NegociacaoController {        //CRIA UM CONTROLADOR PARA INTERAÇÃO COM U
 
         let data = new Date(
             ...this._inputData.value.split('-') //TRANSFORMA A STRING EM UM ARRAY DE TRÊS ELEMENTOS
-            .map(function(item,indice){         //MAPEIA OS ITENS DA STRING E ATUA SOBRE ELES
-                if(indice==1)                   //VERIFICA SE O INDICE É 1 (INDICE DO MES)
-                    return item-1;              //REDUZ 1, DEVIDO A PARTICULARIDADE DO MES DA FUNÇÃO
-                else
-                    return item;                //RETORNA O PROPRIO ITEM CASO NÃO SEJA O MES
-                
-                /*
-                    também podemos remover o if else e usar "return item = item - indice % 2;"
-                */
-            })
+            .map((item,indice) => item = item - indice % 2) //ARROW FUNCTION QUE DÁ O RETORNO DO RESULTADO ITEM
         );
 
         console.log(data);
