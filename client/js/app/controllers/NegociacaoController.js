c0c9c3b6f40d476a4ffe5bf1d93d2e32cae6f561
Matheus Narito Yoneoka <matheus.yoneoka@hotmail.com>
2018-04-03 16:54:22 -0300

    Chamando a negociação dentro do controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -13,25 +13,19 @@ class NegociacaoController {        //CRIA UM CONTROLADOR PARA INTERAÇÃO COM U
     adiciona(event) {           //CRIA UM MÉTODO QUE RECEBE COMO ARGUMENTO O EVENTO UTILIZADO NO 
         
         event.preventDefault();                    //EVITA O RELOAD, AÇÃO PADRÃO DO EVENTO
-        
-        console.log(typeof(this._inputData.value));
-
-        console.log(this._inputData.value);
 
         let data = new Date(
             ...this._inputData.value.split('-') //TRANSFORMA A STRING EM UM ARRAY DE TRÊS ELEMENTOS
             .map((item,indice) => item = item - indice % 2) //ARROW FUNCTION QUE DÁ O RETORNO DO RESULTADO ITEM
         );
 
-        console.log(data);
-
-        /*let negociacao = new Negociacao(           //CRIA UMA INSTANCIA DA NEGOCIACAO COM OS DADOS DOS CAMPOS
-            this._inputData.value,
+        let negociacao = new Negociacao(           //CRIA UMA INSTANCIA DA NEGOCIACAO COM OS DADOS DOS CAMPOS
+            data,                                  //IMPORTANTE, NÃO USAR _INPUTDATA, USAR O VALOR ALTERADO
             this._inputQuantidade.value,            
-            this._inputValor.valor
+            this._inputValor.value
         );
         
-        console.log(negociacao);*/
+        console.log(negociacao);
 
     }
 
