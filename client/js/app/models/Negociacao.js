ba58b338300c5e0d444c77741c7e8c085cb32ffa
Matheus Narito Yoneoka <matheus.yoneoka@hotmail.com>
2018-04-03 11:28:44 -0300

    Utilizando o  object.freeze() para tornar as instancias imutáveis

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,10 +1,11 @@
 class Negociacao {                 //CLASSE CONTENDO NEGOCIAÇÕES
 
-     constructor(data,quantidade,valor) {               //FUNÇÃO CONSTRUTORA CONTENDO ATRIBUTOS DA CLASSE
-         this._data = data;                  //COMO O NOVO OBJETO PASSARÁ OS ATRIBUTOS DA CLASSE PREENCHEMOS
-         this._quantidade = quantidade;      //OS ATRIBUTOS DO CONSTRUTOR COM AS VARIÁVEIS RECEBIDAS
-         this._valor = valor;                //UTILIZAMOS O UNDERSCORE EM ATRIBUTOS QUE N DEVEM SER ACESSADOS FORA DA CLASSE
-     }
+    constructor(data,quantidade,valor) {               //FUNÇÃO CONSTRUTORA CONTENDO ATRIBUTOS DA CLASSE
+        this._data = data;                  //COMO O NOVO OBJETO PASSARÁ OS ATRIBUTOS DA CLASSE PREENCHEMOS
+        this._quantidade = quantidade;      //OS ATRIBUTOS DO CONSTRUTOR COM AS VARIÁVEIS RECEBIDAS
+        this._valor = valor;                //UTILIZAMOS O UNDERSCORE EM ATRIBUTOS QUE N DEVEM SER ACESSADOS FORA DA CLASSE
+        Object.freeze(this);                //CONGELA A INSTANCIA DO OBJETO, BLOQUEANDO A ALTERAÇÃO DOS ATRIBUTOS
+    }
 
     get volume() {                            //CRIA O MÉTODO OBTÉM VOLUME, UMA FUNÇÃO DA CLASSE NEGOCIACAO
          return this._quantidade * this._valor;  //CALCULA O VOLUME COM BASE NOS VALORES ATUAIS DO OBJETO INSTANCIADO
