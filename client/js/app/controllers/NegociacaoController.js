88fa519238897978eda8da2fb9bfaf91be63a37d
Matheus Narito Yoneoka <matheus.yoneoka@hotmail.com>
2018-04-03 15:42:19 -0300

    Transformando as variáveis que armazenam campos selecionados em atributos

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,19 +1,23 @@
 class NegociacaoController {        //CRIA UM CONTROLADOR PARA INTERAÇÃO COM USUÁRIO E DOM
 
+    constructor () {            //CRIA O CONSTRUTOR COM OS ATRIBUTOS DA CLASSE (EXECUTADO APENAS UMA VEZ NA CRIAÇÃO DA CLASSE)
+
+        let $ = document.querySelector.bind(document);      //CRIA UM ATALHO PARA O QUERY SELECTOR
+
+        this.inputData = $('#data');
+        this.inputQuantidade = $('#quantidade');         //BUSCA OS CAMPOS PELOS SEUS IDENTIFICADORES
+        this.inputValor = $('#valor');
+
+    }
+
     adiciona(event) {           //CRIA UM MÉTODO QUE RECEBE COMO ARGUMENTO O EVENTO UTILIZADO NO 
         
         event.preventDefault();                    //EVITA O RELOAD, AÇÃO PADRÃO DO EVENTO
         console.log('Controller Chamado!');
 
-        let $ = document.querySelector.bind(document);      //CRIA UM ATALHO PARA O QUERY SELECTOR
-
-        let inputData = $('#data');
-        let inputQuantidade = $('#quantidade');         //BUSCA OS CAMPOS PELOS SEUS IDENTIFICADORES
-        let inputValor = $('#valor');
-
-        console.log(inputData.value);
-        console.log(inputQuantidade.value);
-        console.log(inputValor.value);
+        console.log(this.inputData.value);
+        console.log(this.inputQuantidade.value);
+        console.log(this.inputValor.value);
 
     }
 
